372a93c3143787e10911b7e35230539236ca7390
jkllkl <jkliklkl877@gmail.com>
2022-02-26 04:51:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ data/authors/default.md
@@ -0,0 +1,9 @@
+---
+name: jkl
+avatar: /static/images/프로필사진.png
+occupation:
+company:
+email: jkliklkl877@gmail.com
+linkedin:
+github: https://github.com/jkllkl
+---
---
+++ data/siteMetadata.js
@@ -0,0 +1,31 @@
+const siteMetadata = {
+  title: 'jkl blog',
+  author: 'jkl',
+  headerTitle: 'blog',
+  description: '개발 블로그',
+  language: 'ko-KR',
+  siteUrl: '',
+  siteRepo: '',
+  siteLogo: '/static/images/로고이름.png',
+  image: '/static/images/프로필사진.png',
+  socialBanner: '/static/images/social-banner.png',
+  email: 'jkliklkl877@gmail.com',
+  github: 'https://github.com/jkllkl',
+  linkedin: '',
+  locale: 'ko-KR',
+  analytics: {
+    googleAnalyticsId: '(옵션)',
+  },
+  comment: {
+    provider: 'utterances',
+    utterancesConfig: {
+      repo: 'jkllkl/blog',
+      issueTerm: 'title',
+      label: 'Comment 💬',
+      theme: 'github-light',
+      darkTheme: 'github-dark',
+    },
+  },
+}
+
+module.exports = siteMetadata
